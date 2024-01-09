2deb692fc83d5d9615564419213361830f329caa
Gabriel Gonçalves de Aragão <50756530+GabrielGoncalves@users.noreply.github.com>
2024-01-09 17:29:32 -0300

    Update code.py
    Remoção do autocontrole
    Inclusão da configuração de tempo
    Ajuste do arquivo favoritos.json para config.json

--- code.py
+++ code.py
@@ -6,33 +6,35 @@ from time import sleep
 import customtkinter
 import pyautogui
 import webbrowser
+import atexit
 
 
 class FrameLateral(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         self.CriaInterface = None
+        self.FrameSuperior = None
 
         self.label_titulo = customtkinter.CTkLabel(self, text="Acesso Remoto", font=("Calibri Bold", 25),
                                                    justify="center")
-        self.label_titulo.grid(row=5, column=0, padx=30, pady=(10, 15), sticky="ew")
+        self.label_titulo.grid(row=5, column=0, padx=30, pady=(10, 10), sticky="ew")
         self.label_ip = customtkinter.CTkLabel(self, text="IP do Servidor:")
-        self.label_ip.grid(row=6, column=0, padx=30, pady=(10, 0), sticky="ew")
+        self.label_ip.grid(row=6, column=0, padx=30, pady=(13, 0), sticky="ew")
         self.entry_ip = customtkinter.CTkEntry(self, corner_radius=25, justify='center')
-        self.entry_ip.grid(row=7, column=0, padx=(5, 15), pady=(10, 0), sticky="ew")
+        self.entry_ip.grid(row=7, column=0, padx=(5, 15), pady=(13, 0), sticky="ew")
         self.label_usuario = customtkinter.CTkLabel(self, text="Nome de Usuário:")
-        self.label_usuario.grid(row=8, column=0, padx=30, pady=(10, 0), sticky="ew")
+        self.label_usuario.grid(row=8, column=0, padx=30, pady=(13, 0), sticky="ew")
         username = getpass.getuser()
         self.entry_usuario = customtkinter.CTkEntry(self, corner_radius=25, justify="center")
-        self.entry_usuario.grid(row=9, column=0, padx=(5, 15), pady=(10, 0), sticky="ew")
+        self.entry_usuario.grid(row=9, column=0, padx=(5, 15), pady=(13, 0), sticky="ew")
         self.entry_usuario.insert(0, f"{username}@cloud")
         self.label_senha = customtkinter.CTkLabel(self, text="Senha:")
-        self.label_senha.grid(row=10, column=0, padx=30, pady=(10, 0), sticky="ew")
+        self.label_senha.grid(row=10, column=0, padx=30, pady=(13, 0), sticky="ew")
         self.entry_senha = customtkinter.CTkEntry(self, show="*", corner_radius=25, justify='center')
-        self.entry_senha.grid(row=11, column=0, padx=(5, 15), pady=(10, 0), sticky="ew")
+        self.entry_senha.grid(row=11, column=0, padx=(5, 15), pady=(13, 0), sticky="ew")
         self.button_conectar = customtkinter.CTkButton(self, text="Conectar", command=self.conectar, text_color="white",
                                                        corner_radius=15, fg_color="#E4621B", hover_color="#3E3E63")
-        self.button_conectar.grid(row=12, column=0, padx=30, pady=(10, 0), sticky="ew")
+        self.button_conectar.grid(row=12, column=0, padx=30, pady=(13, 0), sticky="ew")
 
         # Configura o enter para validar a conexão
         self.entry_senha.bind("<Return>", lambda event: self.conectar())
@@ -58,11 +60,10 @@ class FrameLateral(customtkinter.CTkFrame):
             except Exception as e:
                 print(f"Erro ao conectar: {e}")
 
-            sleep(3.50)
-           # pyautogui.click(517, 391)
+            tempo_rdp = self.Frame_Superior.exibir_tempo()
+            sleep(float(tempo_rdp))
             pyautogui.write(senha)
             pyautogui.press("enter")
-           # pyautogui.click(580, 534)
 
     @staticmethod
     def remover_aviso_certificado(ip):
@@ -79,7 +80,7 @@ class FrameLateral(customtkinter.CTkFrame):
     @staticmethod
     def criar_arquivo_rdp(ip, usuario):
         pasta_acesso_remoto = "C:\\AcessoRemoto"
-        filename = os.path.join(pasta_acesso_remoto, f"conexao_remota_{ip}.rdp")
+        filename = os.path.join(pasta_acesso_remoto, f"{ip}_conexao_remota.rdp")
         with open(filename, 'w') as file:
             file.write(f"full address:s:{ip}\n")
             file.write(f"username:s:{usuario}\n")
@@ -91,11 +92,62 @@ class FrameLateral(customtkinter.CTkFrame):
 class FrameSuperior(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
+        # faz a exlusão dos arquivo rdp quando finalizar a tela
+        atexit.register(self.sair)
 
-        self.button_saida = customtkinter.CTkButton(self, text=" x ", command=FrameSuperior.sair, width=10, height=15,
-                                                    text_color="white", corner_radius=25, fg_color="transparent",
-                                                    hover_color='#990505', font=("Calibri Bold", 15))
-        self.button_saida.grid(row=0, column=1, padx=10, pady=5, sticky="e")
+        self.variavel_opcao = customtkinter.StringVar(self)
+
+        self.button_saida = customtkinter.CTkLabel(self, text="Tempo para fazer o login:", width=10, height=15,
+                                                    font=("Calibri", 15))
+        self.button_saida.grid(row=0, column=1, padx=(0, 70), pady=(8, 0), sticky="e")
+        self.seletor_de_tempo = customtkinter.CTkOptionMenu(self, variable=self.variavel_opcao, 
+                                                    dropdown_fg_color="#3F5663", width=23, height=20, 
+                                                    button_color="#3E3E63", fg_color="#3E3E63", 
+                                                    button_hover_color="#3F4A63",
+                                                    values=("0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5"))
+        self.seletor_de_tempo.grid(row=0, column=1, padx=(0, 15), pady=(8, 0), sticky="e")
+        self.seletor_de_tempo.configure(command=lambda value:self.atualiza_opcoes(self.variavel_opcao, self.salvar_tempo))
+
+    @staticmethod
+    def atualiza_opcoes(variavel_opcao, salvar_tempo):
+        valor_selecionado = variavel_opcao.get()
+        salvar_tempo() 
+
+    def salvar_tempo(self):
+        pasta_config_tempo = "C:\\AcessoRemoto\\Dados"
+        arquivo_config_tempo = os.path.join(pasta_config_tempo, "config.json")
+
+        texto = self.variavel_opcao.get()
+
+        if os.path.exists(arquivo_config_tempo):
+            with open(arquivo_config_tempo, "r") as file:
+                dados_existente = json.load(file)
+        else:
+            dados_existente = {}
+
+        dados_existente["tempo"] = texto
+
+        with open(arquivo_config_tempo, "w") as file:
+            json.dump(dados_existente, file)
+
+    @staticmethod
+    def ler_arquivo_tempo():
+        pasta_acesso_remoto = "C:\\AcessoRemoto\\Dados"
+        arquivo_config_tempo = os.path.join(pasta_acesso_remoto, "config.json")
+
+        if os.path.exists(arquivo_config_tempo):
+            with open(arquivo_config_tempo, "r") as file:
+                dados = json.load(file)
+            return dados
+        return None
+
+    def exibir_tempo(self):
+        texto_tempo = self.ler_arquivo_tempo()
+
+        if texto_tempo and "tempo" in texto_tempo:
+            tempo_rdp = texto_tempo["tempo"]
+            self.seletor_de_tempo.set(tempo_rdp)
+            return tempo_rdp
 
     @staticmethod
     def remover_arquivos_rdp():
@@ -108,21 +160,19 @@ class FrameSuperior(customtkinter.CTkFrame):
     def sair():
         FrameSuperior.remover_arquivos_rdp()
         pasta_dados = "C:\\AcessoRemoto\\Dados"
-        arquivos_protegidos = ["anotacoes.json", "favoritos.json"]
+        arquivos_protegidos = ["anotacoes.json", "config.json"]
 
         for arquivo in os.listdir(pasta_dados):
             if arquivo in arquivos_protegidos:
                 continue
             os.remove(os.path.join(pasta_dados, arquivo))
-        app.quit()
-
+            app.quit()
 
 class FramePrincipal(customtkinter.CTkTabview):
     def __init__(self, master):
         super().__init__(master)
         self.tela_de_informacao = None
         self.CriaInterface = None
-        self.frame_lateral = None
 
         self.add("Favoritos")
         self.add("Anotações")
@@ -187,7 +237,7 @@ class FramePrincipal(customtkinter.CTkTabview):
         self.frame.grid(row=0, column=0, padx=15, pady=15)
 
         self.titulo = customtkinter.CTkLabel(self.tela_de_informacao,
-                                             text="Versão: 0.0.15, desenvolvido por Gabriel Aragão",
+                                             text="Versão: 0.0.30, desenvolvido por Gabriel Aragão",
                                              font=("Calibri", 13), fg_color="#2B2B2B")
         self.titulo.grid(row=0, column=0, padx=15, pady=(0, 50))
         self.dados = customtkinter.CTkLabel(self.tela_de_informacao,
@@ -239,7 +289,6 @@ class FramePrincipal(customtkinter.CTkTabview):
 
         with open(arquivo_anotacoes, "w") as file:
             json.dump({"anotacoes": texto}, file)
-
     @staticmethod
     def exibir_anotacoes(self):
         texto_anotacoes = self.ler_arquivo_anotacoes()
@@ -247,7 +296,6 @@ class FramePrincipal(customtkinter.CTkTabview):
         if texto_anotacoes:
             self.tab2.delete(1.0, "end")
             self.tab2.insert("end", texto_anotacoes)
-
     @staticmethod
     def ler_arquivo_anotacoes():
         pasta_anotacoes = "C:\\AcessoRemoto\\Dados"
@@ -261,7 +309,7 @@ class FramePrincipal(customtkinter.CTkTabview):
 
     def salvar_favoritos(self):
         pasta_favoritos = "C:\\AcessoRemoto\\Dados"
-        arquivo_favoritos = os.path.join(pasta_favoritos, "favoritos.json")
+        arquivo_favoritos = os.path.join(pasta_favoritos, "config.json")
 
         texto1 = self.camp_fav1.get()
         texto2 = self.camp_fav2.get()
@@ -274,7 +322,6 @@ class FramePrincipal(customtkinter.CTkTabview):
 
         with open(arquivo_favoritos, "w") as file:
             json.dump(dados, file)
-
     @staticmethod
     def exibir_favoritos(self):
         dados_favoritos = self.ler_arquivo_favoritos()
@@ -294,11 +341,10 @@ class FramePrincipal(customtkinter.CTkTabview):
                 self.camp_fav3.delete(0, "end")
                 self.camp_fav3.insert(0, dados_favoritos["fav3"])
                 self.camp_fav3.configure(state="disabled")
-
     @staticmethod
     def ler_arquivo_favoritos():
         pasta_favoritos = "C:\\AcessoRemoto\\Dados"
-        arquivo_favoritos = os.path.join(pasta_favoritos, "favoritos.json")
+        arquivo_favoritos = os.path.join(pasta_favoritos, "config.json")
 
         if os.path.exists(arquivo_favoritos):
             with open(arquivo_favoritos, "r") as file:
@@ -331,15 +377,38 @@ class FramePrincipal(customtkinter.CTkTabview):
                         subprocess.Popen(['mstsc', arquivo_rdp])
                         app.lower()
 
-                        sleep(3.75)
-                        pyautogui.click(517, 391)
+                        tempo_rdp = self.Frame_Superior.exibir_tempo()
+                        sleep(float(tempo_rdp))
                         pyautogui.write(senha)
-                        pyautogui.click(580, 534)
+                        pyautogui.press("enter")
 
                     except Exception as e:
                         print(f"Erro ao conectar: {e}")
                 else:
-                    print("Favorito não configurado corretamente.")
+                    self.tela_de_notificacao = customtkinter.CTkToplevel(self)
+                    self.tela_de_notificacao.title("Informações")
+                    self.tela_de_notificacao.focus_set()
+                    self.tela_de_notificacao.grab_set()
+                    self.tela_de_notificacao.resizable(width=False, height=False)
+                    self.tela_de_notificacao.overrideredirect(True)
+            
+                    largura_janela = 280
+                    altura_janela = 110
+            
+                    # ajuste para aparecer no centro da tela principal
+                    largura_tela = self.tela_de_notificacao.winfo_screenwidth()
+                    altura_tela = self.tela_de_notificacao.winfo_screenheight()
+                    pos_x = (largura_tela // 2) - (largura_janela // 2)
+                    pos_y = (altura_tela // 2) - (altura_janela // 2)
+                    self.tela_de_notificacao.geometry(f"{largura_janela}x{altura_janela}+{pos_x}+{pos_y}")
+
+                    self.mensagem_fav = customtkinter.CTkLabel(self.tela_de_notificacao, text="Favorito não configurado corretamente",
+                                                                font=("Calibri", 16))
+                    self.mensagem_fav.grid(row=0, column=0, padx=15, pady=20)
+                    self.bnt_mensagem_fav = customtkinter.CTkButton(self.tela_de_notificacao, text="OK",
+                                            command=self.tela_de_notificacao.destroy,
+                                            corner_radius=15, hover_color="#3E3E63", fg_color="#c75416")
+                    self.bnt_mensagem_fav.grid(row=1, column=0, padx=15, pady=(0, 10))
 
 
 class CriarInterface(customtkinter.CTk):
@@ -351,10 +420,12 @@ class CriarInterface(customtkinter.CTk):
         self.resizable(width=False, height=False)
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=1)
+        self.attributes('-alpha', 0.96)
+        customtkinter.set_appearance_mode("Dark")
 
         self.sidebar_frameL = FrameLateral(self)
         self.sidebar_frameL.CriaInterface = self
-        self.sidebar_frameL.grid(row=1, column=0, padx=(20, 0), pady=(18, 15), sticky="nsw")
+        self.sidebar_frameL.grid(row=1, column=0, padx=(15, 0), pady=(18, 15), sticky="nsw")
         self.sidebar_frameS = FrameSuperior(self)
         self.sidebar_frameS.grid(row=0, column=1, sticky="e")
         self.sidebar_frameS.configure(fg_color="transparent")
@@ -364,21 +435,16 @@ class CriarInterface(customtkinter.CTk):
         self.sidebar_frameP.configure(segmented_button_selected_color="#c75416",
                                       segmented_button_selected_hover_color="#3E3E63")
         self.sidebar_frameP.grid(row=1, column=1, padx=(5, 15), pady=(0, 15), sticky="nswe")
+        self.sidebar_frameL.Frame_Superior = self.sidebar_frameS
+        self.sidebar_frameP.Frame_Superior = self.sidebar_frameS
 
-        customtkinter.set_appearance_mode("Dark")
-
-        self.bind('<Button-1>', self.on_mouse_press)
-        self.bind('<B1-Motion>', self.on_mouse_drag)
-        self.bind('<ButtonRelease-1>', self.on_mouse_release)
-        self.draggable = True  # Correção para movimentar a tela e usar o botão de fechar
-
-        self.overrideredirect(True)
-        self.attributes('-alpha', 0.96)
 
         # chamadas
         self.criar_pastas()
         FramePrincipal.exibir_anotacoes(self.sidebar_frameP)
         FramePrincipal.exibir_favoritos(self.sidebar_frameP)
+        FrameSuperior.exibir_tempo(self.sidebar_frameS)
+
 
     def mensagem_de_alertas(self):
         self.tela_de_alerta = customtkinter.CTkToplevel(self)
@@ -386,6 +452,7 @@ class CriarInterface(customtkinter.CTk):
         self.tela_de_alerta.focus_set()
         self.tela_de_alerta.grab_set()
         self.tela_de_alerta.resizable(width=False, height=False)
+        self.tela_de_alerta.overrideredirect(True)
 
         largura_janela = 245
         altura_janela = 100
@@ -414,18 +481,6 @@ class CriarInterface(customtkinter.CTk):
         if not os.path.exists(pasta_acesso_remoto):
             os.makedirs(pasta_acesso_remoto)
 
-    def on_mouse_press(self, event):
-        self.start_x = event.x_root - self.winfo_x()
-        self.start_y = event.y_root - self.winfo_y()
-
-    def on_mouse_drag(self, event):
-        x = event.x_root - self.start_x
-        y = event.y_root - self.start_y
-        self.geometry(f"+{x}+{y}")
-
-    def on_mouse_release(self, event):
-        self.start_x = None
-        self.start_y = None
 
 
 if __name__ == "__main__":
